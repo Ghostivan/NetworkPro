474105ffed0b02bdf133751e36b70b580383cf56
denam60 <denam60@hotmail.fr>
2016-05-14 12:18:27 +0200

    test plannif

---
+++ 2- Code source/testPlannifConsole/testPlannifConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.Win32.TaskScheduler;
+
+namespace testPlannifConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //TaskS ts = new TaskS("notepad", @"C:\WINDOWS\system32\notepad.exe", new DateTime(2016, 4, 10, 20, 01, 00), "Pour tester lancement du bloc note");
+            TaskS ts = new TaskS("SpotifyInstaller", @"C:\Users\Damien\Downloads\SpotifySetup.exe", new DateTime(2016, 4, 25, 00, 56, 00), "Pour tester l'installation de spotify");
+            ts.listTask();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2- Code source/testPlannifConsole/testPlannifConsole/TaskS.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TaskScheduler;
+
+public class TaskS
+{
+	public TaskS(String name, String path, DateTime dateExec, String description)
+	{
+        if (dateExec < DateTime.Now)
+            throw new SystemException("Date non valide : Date anterieur à la date actuelle");
+        TaskScheduler.TaskScheduler scheduler = new TaskScheduler.TaskScheduler();
+        scheduler.Connect(null, null, null, null); //run as current user.
+
+        ITaskDefinition taskDef = scheduler.NewTask(0);
+        taskDef.RegistrationInfo.Author = "Admin";
+        taskDef.RegistrationInfo.Description = description;
+        //taskDef.Settings.ExecutionTimeLimit = "PT10M"; // 10 minutes
+        taskDef.Settings.DisallowStartIfOnBatteries = false;
+        taskDef.Settings.StopIfGoingOnBatteries = false;
+        taskDef.Settings.WakeToRun = true;
+
+        ITimeTrigger trigger = (ITimeTrigger)taskDef.Triggers.Create(_TASK_TRIGGER_TYPE2.TASK_TRIGGER_TIME);
+
+        DateTime nextRun = dateExec;//DateTime nextRun = DateTime.Now.AddMinutes(2); // one minute from now
+        trigger.StartBoundary = nextRun.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+
+        IExecAction action = (IExecAction)taskDef.Actions.Create(_TASK_ACTION_TYPE.TASK_ACTION_EXEC);
+        action.Id = name;
+        action.Path = path;
+        //action.WorkingDirectory = "working dir";
+        //action.Arguments = "app arguments";  /// <-- here you put your arguments..
+
+        ITaskFolder root = scheduler.GetFolder("\\");
+
+        IRegisteredTask regTask = root.RegisterTaskDefinition(
+            name,
+            taskDef,
+            (int)_TASK_CREATION.TASK_CREATE_OR_UPDATE,
+            null, // user
+            null, // password
+            _TASK_LOGON_TYPE.TASK_LOGON_INTERACTIVE_TOKEN, //User must already be logged on. The task will be run only in an existing interactive session.
+            "" //SDDL
+            );
+	}
+
+    public void listTask()
+    {
+        TaskScheduler.TaskScheduler scheduler = new TaskScheduler.TaskScheduler();
+        scheduler.Connect(null, null, null, null);
+        IRegisteredTaskCollection tsks = scheduler.GetFolder("\\").GetTasks(0);
+        
+        foreach (IRegisteredTask tsk in tsks)
+            Console.WriteLine(tsk.Name);
+
+    }
+}
